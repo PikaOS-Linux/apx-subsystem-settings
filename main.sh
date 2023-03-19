ebbfe5eb5b496a9af810651d283eae95ac382800
Ward Nakchbandi <hotrod.master@hotmail.com>
2023-03-19 18:06:12 +0300

    fix issue

--- main.sh
+++ main.sh
@@ -1,5 +1,3 @@
-DEBIAN_FRONTEND=noninteractive
-
 # Add dependent repositories
 wget -q -O - https://ppa.pika-os.com/key.gpg | sudo apt-key add -
 add-apt-repository https://ppa.pika-os.com
@@ -11,8 +9,6 @@ cp -rvf ./* ./apx-subsystem-settings || echo
 cd ./apx-subsystem-settings
 
 # Get build deps
-ln -fs /usr/share/zoneinfo/America/New_York /etc/localtime
-DEBIAN_FRONTEND=noninteractive apt-get install -y tzdata
 apt-get build-dep ./ -y
 
 # Build package
@@ -22,4 +18,3 @@ dpkg-buildpackage
 cd ../
 mkdir -p ./output
 mv ./*.deb ./output/
-
